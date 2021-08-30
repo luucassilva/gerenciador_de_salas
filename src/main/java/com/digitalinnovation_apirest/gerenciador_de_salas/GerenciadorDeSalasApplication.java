f8560ae6026e0f43c2cc638f62ab2ca0ad249b4c
Lucas Silva <lucascavalcante@hotmail.com>
2021-08-29 22:46:18 -0300

    project creation

---
+++ src/main/java/com/digitalinnovation_apirest/gerenciador_de_salas/GerenciadorDeSalasApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation_apirest.gerenciador_de_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeSalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation_apirest/gerenciador_de_salas/GerenciadorDeSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation_apirest.gerenciador_de_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
